1e6a83bbb1b0f24495c83f76d01a93a4ff35d65a
bonghoadepnhat <18020331@vnu.edu.vn>
2023-05-08 11:12:01 +0700

    move to vitejs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx}'],
-    theme: {
-        extend: {
-            colors: {
-                primary: '#8dc647',
-            },
-            borderColor: {
-                primary: '#8dc647',
-            },
-            backgroundColor: {
-                primary: '#8dc647',
-            },
-            boxShadow: {
-                headerSearch: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',
-            },
-        },
-    },
-    plugins: [],
+export default {
+   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+   theme: {
+      extend: {
+         colors: {
+            primary: '#8dc647',
+         },
+         borderColor: {
+            primary: '#8dc647',
+         },
+         backgroundColor: {
+            primary: '#8dc647',
+         },
+         boxShadow: {
+            headerSearch: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',
+         },
+      },
+   },
+   plugins: [],
 }
