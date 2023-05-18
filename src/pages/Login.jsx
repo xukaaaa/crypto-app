d8a5297c92cf2ebcd6ee58b9d11e1ef64b95e1c5
Thai Nguyen Hoang <thai03040302@gmail.com>
2023-05-18 14:25:13 +0700

    Login and Register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,79 @@
 import React, { useState } from 'react'
 import GithubAuthButton from '../components/Button/GithubAuthButton'
 import GoogleAuthButton from '../components/Button/GoogleAuthButton'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { signInWithEmailAndPassword, sendEmailVerification } from 'firebase/auth'
+import { auth } from '../firebase'
+import { toast } from 'react-toastify'
 
 function Login() {
    const [userName, setUserName] = useState('');
    const [password, setPassword] = useState('');
+   const [isLoading, setIsLoading] = useState(false);
+   const navigate = useNavigate();
+   const handleLogin = (e) => {
+      setIsLoading(true);
+      e.preventDefault();
+      signInWithEmailAndPassword(auth, userName, password)
+         .then(() => {
+            toast.success('Login success!');
+            setTimeout(() => {
+               navigate('/')
+            }, 1000);
+         })
+         .catch(e => {
+            console.log(e.code);
+            switch (e.code) {
+               case "auth/user-not-found":
+                  toast.error("Wrong email!");
+                  break;
+               case "auth/invalid-email":
+                  toast.error("Invalid email!");
+                  break;
+               case "auth/wrong-password":
+                  toast.error("Wrong password!");
+                  break;
+               case "auth/too-many-requests":
+                  toast.error("Too many attemp! Try again later.");
+                  break;
+               default:
+                  toast.error('Login failed!');
+            }
+            setIsLoading(false);
+         })
+
+   }
    return (
       <>
          <div className="relative left-[667px] mt-[245px] w-[104px] h-[48px] font-inter font-normal text-[40px] leading-[48px] text-white">Login</div>
          <div className=" relative left-[370px] top-[25px] mb-[168px] flex relative w-[700px] h-[704px] bg-[#1E1B3E] rounded-[20px]" >
             <div className="absolute left-[90px] top-[82px] font-inter font-normal text-[20px] leading-[24px] text-white">Username</div>
-            <input
-               className="left-[76px] top-[116px] box-border absolute w-[547px] h-[42px] border-[2px] border-solid border-[#30215A] rounded-[10px]"
-               type='text'
-               value={userName}
-               onChange={(even) => {
-                  setUserName(even.target.value);
-               }}
-            ></input>
-            <div className="absolute left-[90px] top-[189px] font-inter font-normal text-[20px] leading-[24px] text-white">Password</div>
-            <input
-               type='Password' className="left-[76px] top-[231px] box-border absolute w-[547px] h-[42px] border-[2px] border-solid border-[#30215A] rounded-[10px]"
-               value={password}
-               onChange={(even) => {
-                  setPassword(even.target.value);
-               }}
-            ></input>
-            <button className="absolute w-[252px] h-[65px] bg-[#6638E5] rounded-[30px] font-itim font-normal text-[28px] leading-[34px] left-[244px] top-[316px]">Login</button>
+            <form onSubmit={handleLogin}>
+               <input
+                  required={true}
+                  className="left-[76px] top-[116px] box-border absolute w-[547px] h-[42px] border-[2px] border-solid border-[#30215A] rounded-[10px]"
+                  type='text'
+                  value={userName}
+                  onChange={(even) => {
+                     setUserName(even.target.value);
+                  }}
+               ></input>
+               <div className="absolute left-[90px] top-[189px] font-inter font-normal text-[20px] leading-[24px] text-white">Password</div>
+               <input
+                  required={true}
+                  type='Password' className="left-[76px] top-[231px] box-border absolute w-[547px] h-[42px] border-[2px] border-solid border-[#30215A] rounded-[10px]"
+                  value={password}
+                  onChange={(even) => {
+                     setPassword(even.target.value);
+                  }}
+               ></input>
+
+               <button
+                  disabled={isLoading}
+                  type='submit'
+                  className="absolute w-[252px] h-[65px] bg-[#6638E5] rounded-[30px] font-itim font-normal text-[28px] leading-[34px] left-[244px] top-[316px]"
+               >Login</button>
+            </form>
             <Link
                className="absolute w-[230px] h-[27px] font-inter font-normal text-[22px] leading-[27px] text-white 
                            left-[235px] top-[416px] hover:text-hoverPrimary"
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,35 +1,74 @@
 import React, { useState } from 'react'
 import GoogleAuthButton from '../components/Button/GoogleAuthButton'
 import GithubAuthButton from '../components/Button/GithubAuthButton'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { auth } from '../firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth'
+import Cookies from 'js-cookie'
+import { useDispatch } from 'react-redux'
+import { login, logout } from '../redux/authSlice'
+import { toast } from 'react-toastify'
 
 function Register() {
    const [userName, setUserName] = useState('');
    const [password, setPassword] = useState('');
    const [rePassword, setRePassword] = useState('');
    const [status, setStatus] = useState('');
+   const [isLoading, setIsLoading] = useState(false);
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
    const validate = () => {
       let passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-      if (!password.match(passw)) {
-         alert('Password between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter.');
-         return false;
-      }
-      if (password !== rePassword) { 
-         alert('Password not match.');
+      // if (!password.match(passw)) {
+      //    toast.error('Password between 6 to 20 characters which contain at least one numeric digit, one uppercase and one lowercase letter.');
+      //    return false;
+      // }
+      if (password !== rePassword) {
+         toast.error('Password and comfirm password not match.');
          return false;
       }
       return true
    }
    const register = (e) => {
+      // setIsLoading(true);
       e.preventDefault();
       if (validate()) {
-         createUserWithEmailAndPassword(auth,userName,password)
-         .then((res)=>{
-            console.log(res.user);
-         })
-         .catch(err => console.log(err))
+         createUserWithEmailAndPassword(auth, userName, password)
+            .then((res) => {
+               console.log(res.user);
+               const { email, photoURL, uid } = res.user
+               const displayName = email;
+               Cookies.set(
+                  'user',
+                  JSON.stringify({ displayName, email, photoURL, uid }),
+                  { secure: true }
+               )
+               dispatch(login({ displayName, email, photoURL, uid }));
+               toast.success('Register success!');
+               setTimeout(() => {
+                  navigate('/')
+               }, 1000);
+            })
+            .catch(e => {
+               console.log(e.code);
+               switch (e.code) {
+                  case "auth/weak-password":
+                     toast.error("The password must be 6 characters long or more.");
+                     break;
+                  case "auth/invalid-email":
+                     toast.error("Invalid email!");
+                     break;
+                  case "auth/email-already-in-use":
+                     toast.error("The email address is already in use by another account.");
+                     break;
+                  case "auth/too-many-requests":
+                     toast.error("Too many attemp! Try again later.");
+                     break;
+                  default:
+                     toast.error('Register failed!');
+               }
+               // setIsLoading(false);
+            })
       }
    }
    return (
@@ -72,13 +111,11 @@ function Register() {
                <button
                   className="absolute w-[252px] h-[65px] bg-[#6638E5] rounded-[30px] font-itim font-normal text-[28px] leading-[34px] left-[225px] bottom-[324px]"
                   type='submit'
+                  disabled={isLoading}
 
                >REGISTER</button>
             </form>
-            <div
-               className="absolute font-inter font-normal text-[22px] leading-[27px] text-white 
-                           left-[235px] top-[416px] hover:text-hoverPrimary"
-            >{status}</div>
+
             <div
                className="absolute font-inter font-normal text-[16px] leading-[19px] text-white left-[289px] bottom-[191px]"
 
